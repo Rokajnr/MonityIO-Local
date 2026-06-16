76642dd4f65944cd892c34eec7bb9a72c6dc9a64
Pike Msonda <pike.msonda@gmail.com>
2026-06-16 18:05:44 +0300

    added favicon and manifest

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "MonityIO",
   description: "",
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
